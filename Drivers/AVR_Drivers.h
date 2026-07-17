95f8c8c5f7c85f94bce4c4b9271930f09450e095
Anders <anders.bjoerk.unf@gmail.com>
2026-07-17 22:54:48 +0200

    first commit

---
+++ Drivers/AVR_Drivers.h
@@ -0,0 +1,12 @@
+/**
+ * @file AVR_Drivers.h
+ * @author Anders Bjørk (anders.bjoerk.unf@gmail.com)
+ * @brief Interface access to the AVR drivers.
+ * @version 0.1
+ * @date 2026-07-17
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#pragma once
+
---
+++ Drivers/PIN/src/pin.c
@@ -0,0 +1,15 @@
+/**
+ * @file pin.c
+ * @author Anders Bjørk (anders.bjoerk.unf@gmail.com)
+ * @brief Function description for the Pin driver.
+ * @version 0.1
+ * @date 2026-07-17
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include "pin.h"
+
+void setPinMode(uint8_t pin, PinMode_e mode) {
+
+}
