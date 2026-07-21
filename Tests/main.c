473b7bce60908e83541fd275d18aa646fd725adb
Anders <anders.bjoerk.unf@gmail.com>
2026-07-21 19:36:29 +0200

    [ADD] added new test folder and main file

---
+++ Tests/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.c
+ * @author Anders Bjørk (anders.bjoerk.unf@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2026-07-21
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include "Atmega.h"
+#include "AVR_Drivers.h"
+
+int main (void) {
+    
+    while (1) {
+        // Main loop
+    }
+    return 0;
+}
